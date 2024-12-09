b67c974e1d14c7ae62872234e07b90f2025a0081
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2024-12-09 15:38:55 -0300

    test: chante tests to new pattern

--- src/app/api/v1/status/get.test.ts
+++ src/app/api/v1/status/get.test.ts
@@ -1,27 +1,30 @@
-import { expect, test, beforeAll } from '@jest/globals';
 import orchestrator from '../../orchestrator/orchestrator';
 
 beforeAll(async () => {
   orchestrator.waitForAllServices();
 });
 
-test('should return status', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/status');
+describe('GET /api/v1/status', () => {
+  describe('Anonymous user', () => {
+    test('Retrieving current status', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/status');
 
-  expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-  const body = await response.json();
+      const body = await response.json();
 
-  const parseUpdatedAt = new Date(body.updated_at).toISOString();
+      const parseUpdatedAt = new Date(body.updated_at).toISOString();
 
-  expect(body).toEqual({
-    updated_at: parseUpdatedAt,
-    dependencies: {
-      database: {
-        version: '16.4',
-        max_connections: 100,
-        opened_connections: 0,
-      },
-    },
+      expect(body).toEqual({
+        updated_at: parseUpdatedAt,
+        dependencies: {
+          database: {
+            version: '16.4',
+            max_connections: 100,
+            opened_connections: 0,
+          },
+        },
+      });
+    });
   });
 });
