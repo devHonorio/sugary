a60fcd70d181c473bca0b7400306c1c3af1c82f1
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2024-12-09 16:11:27 -0300

    feat: add method `PATCH` in endpoint `/api/v1/clients/:id`

---
+++ src/app/api/v1/clients/[id]/patch.test.ts
@@ -0,0 +1,64 @@
+import orchestrator from 'src/app/api/orchestrator/orchestrator';
+
+beforeAll(async () => {
+  orchestrator.waitForAllServices();
+  await orchestrator.cleanDatabase();
+  await orchestrator.seedDatabase.seedClient();
+});
+
+describe('PATCH /api/v1/clients/:id', () => {
+  describe('Anonymous user', () => {
+    test('Updating client name', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/clients/1', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: 'John Doe' }),
+      });
+
+      expect(response.status).toBe(200);
+    });
+
+    test('Updating client phone', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/clients/1', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ phone: '0123456789' }),
+      });
+
+      expect(response.status).toBe(200);
+    });
+
+    test('Updating client not found', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/clients/2', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: 'John Doe' }),
+      });
+
+      expect(response.status).toBe(404);
+    });
+
+    test('Updating client with invalid data', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/clients/1', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      expect(response.status).toBe(400);
+
+      const body = await response.json();
+
+      expect(body).toEqual({
+        errors: [{ message: 'requisição deve conter um body' }],
+      });
+    });
+  });
+});
--- src/app/api/v1/clients/[id]/route.ts
+++ src/app/api/v1/clients/[id]/route.ts
@@ -1,4 +1,5 @@
 import { client } from 'src/infra/prisma';
+import { z } from 'zod';
 
 interface Params {
   params: { id: string };
@@ -28,6 +29,45 @@ export const GET = async (_req, { params }: Params) => {
     }
     return Response.json(response);
   } catch {
+    return new Response(null, { status: 500 });
+  }
+};
+
+const schemaClient = z.object({
+  name: z.string().min(3).max(255).optional(),
+  phone: z.string().min(10).max(11).optional(),
+});
+export const PATCH = async (req: Request, { params }: Params) => {
+  try {
+    const data = await req.json();
+
+    const { name, phone } = schemaClient.parse(data);
+
+    await client.client.update({
+      where: { id: params.id },
+      data: { name, phone },
+    });
+    return Response.json(null, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response(JSON.stringify({ errors: error.errors }), {
+        status: 400,
+      });
+    }
+    if (error instanceof Error) {
+      if (error.message.search('not found') !== -1) {
+        return new Response(null, { status: 404 });
+      }
+      if (error.message.search('Unexpected end of JSON input') !== -1) {
+        return new Response(
+          JSON.stringify({
+            errors: [{ message: 'requisição deve conter um body' }],
+          }),
+          { status: 400 },
+        );
+      }
+    }
+
     return new Response(null, { status: 500 });
   }
 };
